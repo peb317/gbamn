46e5888f20221e79c6b1a4465ab11fd31d9b154d
peb317 <peb317@naver.com>
2016-01-13 04:44:03 +0900

    Add dbconfig.php

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -3,7 +3,7 @@ if (!defined('_GNUBOARD_')) exit;
 define('G5_MYSQL_HOST', 'localhost');
 define('G5_MYSQL_USER', 'root');
 define('G5_MYSQL_PASSWORD', 'apmsetup');
-define('G5_MYSQL_DB', 'gbamn');
+define('G5_MYSQL_DB', 'gbamn_mysql');
 define('G5_MYSQL_SET_MODE', false);
 
 define('G5_TABLE_PREFIX', 'g5_');
